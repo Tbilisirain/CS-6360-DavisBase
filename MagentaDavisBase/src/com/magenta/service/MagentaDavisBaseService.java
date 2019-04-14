8f13c5a5b17d9bb165315e1596d6d9010d6f267f
TOM <supercell@TOMs-MacBook-Pro.local>
2019-04-14 17:22:34 -0500

    add service class

---
+++ MagentaDavisBase/src/com/magenta/service/MagentaDavisBaseService.java
@@ -0,0 +1,12 @@
+package com.magenta.service;
+
+import java.util.List;
+
+public class MagentaDavisBaseService {
+
+	public void executeQuery(List<String> columnList, String tableName, String condition) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
