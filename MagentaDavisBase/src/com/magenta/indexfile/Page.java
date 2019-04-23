43506f07985a31eed9f0fce41a924c2c222a26f4
Arjit <arjit32754@gmail.com>
2019-04-23 13:29:03 -0500

    updating with createPage and createLeafPage methods.

--- MagentaDavisBase/src/com/magenta/indexfile/Page.java
+++ MagentaDavisBase/src/com/magenta/indexfile/Page.java
@@ -1,5 +1,6 @@
 package com.magenta.indexfile;
 import com.magenta.tablefile.*;
+import java.io.RandomAccessFile;
 /*
 This file will have followtng methods:
 createPage(), params : File
@@ -12,6 +13,7 @@ public class Page {
 	byte indexType;
 	byte indexValue;
 	byte[] rowids;
+	public static int pageSize = 512;
 	public void Page() {
 		
 	}
@@ -22,6 +24,30 @@ public class Page {
 		
 	}
 
+	public static int createPage(RandomAccessFile file) {
+		int pages = 0;
 	
+			pages = (int)(file.length()/(new Long(pageSize)));
+			pages = pages + 1;
+			file.setLength(pageSize * pages);
+			file.seek((pages-1) * pageSize);
+			file.writeByte(0x05);  
+		
+		return pages;
+	}
+
+	public static int createLeafPage(RandomAccessFile file) {
+		int pages = 0;
+	
+			pages = (int)(file.length()/(new Long(pageSize)));
+			pages = pages + 1;
+			file.setLength(pageSize * pages);
+			file.seek((pages-1) * pageSize);
+			file.writeByte(0x0D);
+
+		return pages;
+	}
+
+
 
 }
