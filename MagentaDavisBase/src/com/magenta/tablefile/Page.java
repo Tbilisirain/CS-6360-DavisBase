6671c3aa1c102cc92040f3f79f5fa366e7e65756
Arjit <arjit32754@gmail.com>
2019-04-23 13:22:18 -0500

    required methods

--- MagentaDavisBase/src/com/magenta/tablefile/Page.java
+++ MagentaDavisBase/src/com/magenta/tablefile/Page.java
@@ -3,6 +3,18 @@ package com.magenta.tablefile;
 public class Page {
 	Header h;
 	//Leaf level info
+	public static final int pageSize = 512;
+	/*
+	This file should contain table related methods namely:
+	show()
+	showDatabase()
+	drop(), params : table, database
+	dropDatabase(), params: database
+	createDatabase(), params: database
+	getPayload(), params: file, location as explained by professor today.
+	createTable(), params: table, cols
+	insert(), params: table, values
+	*/
 	byte noOfColumns;
 	byte dataTypes[];
 	byte Payload[];// Need to define the payload size based on size of the page.
